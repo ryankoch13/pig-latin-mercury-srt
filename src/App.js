99cb9c2e287d061893284043ad241205c024abf5
K8Sewell <kait@kaitsewell.com>
2019-10-06 18:20:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      phrase: '',
+      phraseTranslated: 'This is where your translated sentence will appear.'
+    }
+  }
+  
+  // The translate function is where you will put your logic to convert the sentence entered by the user to pig location.  What is currently in the function will only directly copy what the user has entered.
+
+  translate = (e) => {
+    e.preventDefault()
+    let translated = this.state.phrase
+    this.setState({phraseTranslated: translated})
+  }
+
+  handleChange = (e) => {
+    this.setState({phrase: e.target.value})
+  }
+
+  render() {
+    return (
+      
+      <div className="wrapper">
+        <header className="box header">
+          <div id="pigImage">
+            <img src='https://lh3.googleusercontent.com/QvvsRY5ShwDNEouVMK8_z7QCwS3grkgd4mzZOlom23Hurralk54ObvsyEMM8ZSNR5pEFBeBMzltzEEcgi2llYJnhXTuXClN3njmMjtw3vgn8Go5jr40fHMNzfI64eYRrnHbZUutxCA=w2400' alt="pig with butcher cut names in pig latin" id="butcherPig"></img>
+          </div>
+        </header>
+        <sidebar className="box sidebar">
+          <div>
+            <form className="info" onSubmit={this.translate}>
+              <label htmlFor="input-phrase">Translate this: </label>
+              <input name="input-phrase" onChange={this.handleChange}></input>
+              <input className="button" type="submit" value="Submit" />
+            </form>
+          </div>
+        </sidebar>
+        <main>
+          <div className="text-center box content">
+            <p>{this.state.phraseTranslated}</p>
+          </div>
+        </main>
+        <footer className="box footer">
+          <div className="text-center">
+            <p>Coded by * * *</p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
